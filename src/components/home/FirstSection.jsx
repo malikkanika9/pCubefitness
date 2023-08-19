e85f726907f7dee4fc5bce44480140ef8a72be65
kanika INT <malikkanika9@gmail.com>
2023-08-19 13:42:48 +0530

    ok

--- src/components/home/FirstSection.jsx
+++ src/components/home/FirstSection.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router";
 import styles from "./home.module.css";
-import homme from "../../assets/c1e23fc3-01ce-44c2-98e3-7465cb8194b1.jpg"
+// import homme from "../../assets/c1e23fc3-01ce-44c2-98e3-7465cb8194b1.jpg"
 import ReactPlayer from "react-player"
 const FirstSection = () => {
   const navigate = useNavigate();
@@ -8,9 +8,9 @@ const FirstSection = () => {
   return (
     <div className={styles.first_section} data-aos="zoom-out-down">
       <div >
-       <img src={homme} style={{ width: "100vw", height: "90vh", objectFit: "cover" }}  alt="home" />
-          {/* <source src="" type="video/mp4" /> */}
-      
+       <video src={homme} style={{ width: "100vw", height: "90vh", objectFit: "cover" }} >
+          <source src="https://drive.google.com/uc?export=download&id=1xi7jLL8L0TOyy-1j1vZVds9HKzg1S5JX" type="video/mp4" />
+      </video>
 
         <div className={styles.button_container}>
         
