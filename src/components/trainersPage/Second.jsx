07a741b10773c4b02f62603d97ade4e9686617ba
kanika <kanika@octet.design>
2023-12-11 11:11:29 +0530

    done

--- src/components/trainersPage/Second.jsx
+++ src/components/trainersPage/Second.jsx
@@ -18,18 +18,18 @@ const Second = () => {
             <img src={photo3} alt="trainer" />
             <div className={styles.info}>
               <div>
-                <h4>pCube Fitness Rohtak </h4>
+                <h4>Pcube Fitness Rohtak </h4>
               </div>
-              <h5>13L, Model Town,Rohtak 124001 <br /> <br />Ph: 8059053050</h5>
+              <h5>13L, Model Town,Rohtak 124001 <br /> <br />Ph: 8950892125 </h5>
             </div>
           </div>
           <div data-aos="fade-right">
             <img src={photo2} alt="trainer" />
             <div className={styles.info}>
               <div>
-                <h4>pCube Fitness Delhi</h4>
+                <h4>Pcube Fitness Model Town </h4>
               </div>
-              <h5>F14/51, Model Town, New Delhi 110009 <br /> <br />Ph: 8950892125</h5>
+              <h5>F14/51, Model Town, New Delhi 110009 <br /> <br />Ph: 8059053050</h5>
             </div>
           </div>
           <br />
@@ -37,7 +37,7 @@ const Second = () => {
             <img src={photo1} alt="trainer" />
             <div className={styles.info}>
               <div>
-                <h4>Turf by pCube</h4>
+                <h4>Turf by Pcube</h4>
               </div>
               <h5>Gk-2 M Block, New Delhi 110048<br /><br /> Ph: 8199981009 </h5>
             </div>
@@ -46,7 +46,7 @@ const Second = () => {
             <img src={photo4} alt="trainer" />
             <div className={styles.info}>
               <div>
-                <h4>pCube Pitampura</h4>
+                <h4>Pcube Pitampura</h4>
               </div>
               <h5>B162, Lok Vihar, New Delhi 110034<br /> <br /> Ph: 8014880880</h5>
             </div>
